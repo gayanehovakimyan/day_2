713caa341e1b2bd75fe2e5a0b2fe2a0e90f716f2
Gayane Hovakimyan <139545787+gayanehovakimyan@users.noreply.github.com>
2024-02-25 02:15:04 +0400

    Add files via upload

---
+++ toggle button text/main.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    var x = document.getElementById("myDiv");
+    if (x.innerHTML ==="Hello"){
+        x.innerHTML = "Good bye";
+        } else {
+            x.innerHTML = "Hello";
+        }
+}
